28700170538d326fb8faee9154a9c73f0589e7ff
anAwesomeWave <timofey.zubov.05@gmail.com>
2022-03-29 14:43:42 +0300

    add 'insert' and 'free_memory' functions!

--- main.c
+++ main.c
@@ -20,7 +20,7 @@ void push_front(struct Node** head, int data) {
         ptr->next = (*head);
         (*head) = ptr;
     }
-};
+}
 
 void pop_front(struct Node** head) {
     if(*head != NULL) {
@@ -31,14 +31,15 @@ void pop_front(struct Node** head) {
 }
 
 void printList(struct Node* h) {
-    /*while(h != NULL) {
+    while(h != NULL) {
         printf("%d ", h->data);
         h = h->next;
-    }*/
-    if(h == NULL)
+    }
+    printf("\n");
+    /*if(h == NULL)
         return;
     printf("%d ", h->data);
-    printList(h->next);
+    printList(h->next);*/
 }
 
 struct Node* get_last_el(struct Node* ptr) {
@@ -49,28 +50,79 @@ struct Node* get_last_el(struct Node* ptr) {
     return get_last_el(ptr->next);
 }
 
-void push_back(struct Node** ptr, int data) {
+struct Node* get_last_but_one(struct Node* head) {
+    if(head == NULL || head ->next == NULL)
+        return NULL;
+    if(head->next->next==NULL)
+        return head;
+    return get_last_but_one(head->next);
+}
+
+void push_back(struct Node** head, int data) {
     struct Node* new_el = (struct Node *)malloc(sizeof(struct Node));
     new_el -> data = data;
     new_el -> next = NULL;
-    if(*ptr == NULL) {
-        (*ptr) = new_el;
+    if(*head == NULL) {
+        (*head) = new_el;
     } else {
-        struct Node* last = get_last_el(*ptr);
+        struct Node* last = get_last_el(*head);
         (*last).next = new_el;
     }
+}
+void pop_back(struct Node** head) {
+    if((*head) != NULL) {
+        if((*head)->next == NULL){
+            free(*head);
+            *head = NULL;
+        } else {
+            struct Node* ptr = (get_last_but_one(*head));
+            free(ptr->next);
+            ptr->next = NULL;
+        }
+
+    }
+}
+
+void insert(struct Node** head, int n, int data) { // 'n' and 'i' are signed because of 'n-1'
+    struct Node* new = (struct Node*) malloc(sizeof(struct Node));
+    new->data = data;
+    if(n == 0) {
+        new->next = (*head);
+        *head = new;
+    }else {
+        int i = 0;
+        struct Node *tmp = (*head);
+        while (i < n - 1 && tmp->next) { // search for previous element
+            tmp = tmp->next;
+            i++;
+        }
+        new->next = tmp->next;
+        tmp->next = new;
+    }
+}
+
+void free_memory(struct Node* head) {
+    if(!head)
+        return;
+    if(head->next)
+        free_memory(head->next);
+    free(head);
 }
 
 int main() {
     struct Node* head = NULL;
-    for(int i = 0; i < 5; i ++) {
+    for(int i = 1; i < 6; i ++) {
         push_back(&head, i);
         printList(head);
-        printf("\n");
     }
-    while(head != NULL) {
-        pop_front(&head);
-        printList(head);
-        printf("\n");
-    }
+    printf("\n");
+    insert(&head, 1, 0);
+    printList(head);
+    insert(&head, 2, 0);
+    printList(head);
+    insert(&head, 0, 0);
+    printList(head);
+    insert(&head, 8, 0);
+    printList(head);
+    free_memory(head);
 }
